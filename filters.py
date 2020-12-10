876fb194d4019a8c2dd8df0178ddea3c9f220e3f
MaximPovolotskii <71864277+MaximPovolotskii@users.noreply.github.com>
2020-12-11 01:03:15 +0300

    Revert "Equalizer"

--- filters.py
+++ filters.py
@@ -1,40 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def spectr_filter(fmax, N, filter_type='peak', f0=-1, peak_a0=0.1, peak_b0=0.1, 
-                  pass_a0=3, pass_h0=1, shelf_h0=1, shelf_a0=3):
+def spectr_filter(fmax, N, filter_type='peak', f0=-1):
     if f0 == -1: # f0 - характерная частота пропускания
-        f0 = fmax//5
+        f0= fmax//5
     numbers = np.arange(N)
-    fn = fmax*(numbers)/(N - 1)
-    
+    fn = fmax*(numbers - 1)/(N - 1)
     if filter_type == 'peak': 
-        #peak_a0 = 0.1 # регулирует высоту всего
-        #peak_b0 = 0.1 # регулирует толщину пика
-        a = np.exp(peak_a0*((np.log(fn) - np.log(f0))**2 + peak_b0)**(-1))
+        a0 = 0.1 # регулирует высоту всего
+        b0 = 0.1 # регулирует толщину пика
+        a = np.exp(a0*((np.log(fn) - np.log(f0))**2 + b0)**(-1))
 
     if filter_type == 'low_pass':
-        #pass_a0 = 3 # a0 > 0, при увеличении a0 увеличивается резкость отрубания
-        #pass h0 влияет на амплитуду
-        a = np.exp(pass_a0*(-np.exp(pass_a0*(np.log(fn)-np.log(f0)))))
+        a0 = 3 # a0 > 0, при увеличении a0 увеличивается резкость отрубания
+        a = np.exp(-np.exp(a0*(np.log(fn)-np.log(f0))))
         
     if filter_type == 'high_pass':
-        #pass_a0 = 3 # a0 < 0, при увеличении модуля a0 увеличивается резкость отрубания
-        #pass h0 влияет на амплитуду
-        a = np.exp(pass_a0*(-np.exp(-pass_a0*(np.log(fn)-np.log(f0)))))
+        a0 = -3 # a0 < 0, при увеличении модуля a0 увеличивается резкость отрубания
+        a = np.exp(-np.exp(a0*(np.log(fn)-np.log(f0))))
         
     if filter_type == 'high_shelf':
-        #shelf_h0 = 1 # > 0, высота полки
-        #shelf_a0 = 3 # > 0, регулирует резкость полки
-        a = np.exp(shelf_h0*(np.tanh(shelf_a0*(np.log(fn)-np.log(f0)))) + 1)
+        h0 = 1 # >0, высота полки
+        a0 = 3 # >0, регулирует резкость полки
+        a = np.exp(h0*(np.tanh(a0*(np.log(fn)-np.log(f0)))) + 1)
         
     if filter_type == 'low_shelf':
-        #shelf_h0 = -1 # < 0 высота полки
-        #shelf_a0 = 3 # > 0 регулирует резкость полки
-        a = np.exp(-shelf_h0*(np.tanh(shelf_a0*(np.log(fn)-np.log(f0)))) + 1)
+        h0 = -1 #<0 высота полки
+        a0 = 3 #>0 регулирует резкость полки
+        a = np.exp(h0*(np.tanh(a0*(np.log(fn)-np.log(f0)))) + 1)
         
     # нарисуем графики для демонстрации
-    '''
     fig, ax = plt.subplots()
     ax.plot(np.log(fn), np.log(a))
     ax.set_title(filter_type)
@@ -46,11 +41,11 @@ def spectr_filter(fmax, N, filter_type='peak', f0=-1, peak_a0=0.1, peak_b0=0.1,
     ax1.set_title(filter_type)
     ax1.set_xlabel('f')
     ax1.set_ylabel('a')
-    '''
+        
     return a
 
-'''
+
 fmax = 100
 N = 1000
 a = spectr_filter(fmax, N, 'low_shelf')
-'''
+
--- fourier.py
+++ fourier.py
@@ -7,36 +7,21 @@ import copy
 def fourier_transform(*tracks_with_durations): #[track, duration]
     fig, ax = plt.subplots()
     fig1, ax1 = plt.subplots()
-    #fig2, ax2 = plt.subplots()
+    fig2, ax2 = plt.subplots()
     for channel in tracks_with_durations:
         d_channel = copy.copy(channel[0])
         # Number of samplepoints
         N = len(d_channel)
         # sample spacing
         dT = channel[1] / N
-        yf = scipy.fft.rfft(d_channel)
+        yf = scipy.fft.fft(d_channel)
         tf = np.linspace(0.0, dT*N, N)
         xf = np.linspace(0.0, 1.0/(2.0*dT), N//2)
-        # рисуем график самого отрезка
+        #рисуем график самого отрезка
         ax.plot(tf, d_channel)
-        ax.set_title('d_channel')
-        # рисуем разложение по спектру
-        ax1.plot(xf[0:N//2], 2.0/N * np.abs(yf[0:N//2])) 
-        ax1.set_title('spectrum')                          
-        #zf = scipy.fft.ifft(yf) # в идеале эта функция обратно преобразовывает
-        #ax2.plot(tf, zf) # рисуем график обратного преобразования
-        
+        #рисуем разложение по спектру
+        ax1.plot(xf[0:N//16], 2.0/N * np.abs(yf[0:N//16]))                           
+        zf = scipy.fft.ifft(yf) # в идеале эта функция обратно преобразовывает
+        ax2.plot(tf, zf) #рисуем график обратного преобразования
     plt.show()
-    return [yf, channel[1]]
 
-def reverse_transform(yf, duration):
-    channel_zf =  scipy.fft.irfft(yf)
-    N = len(channel_zf)
-    dT = duration / N
-    tf = np.linspace(0.0, dT*N, N)
-    fig2, ax2 = plt.subplots()
-    ax2.plot(tf, channel_zf)
-    ax2.set_title('reverse_transform')
-    plt.show()
-    
-    return [channel_zf, duration]
