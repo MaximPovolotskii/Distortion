04562e7897fa29c887e8b2dd663faed70b13d867
MaximPovolotskii <povolotskii.mr@phystech.edu>
2020-12-15 16:31:54 +0300

    Fourier with and without graphics has become distinguished

--- fourier.py
+++ fourier.py
@@ -3,57 +3,109 @@ import matplotlib.pyplot as plt
 import scipy.special
 import copy
 
+
 def smooth(yf):
+    """
+    функция очистки спектра от бесполезных частот с малыми амплитудами
+    """
     yf_sm = copy.copy(yf)
     abs_yf = np.abs(yf)
     max_value = abs_yf.max()
+    
     for i in range(len(yf)):
         if np.abs(yf[i]) < 0.05 * max_value:
            yf_sm[i] = 0
+           
     return yf_sm
 
-def fourier_transform(*tracks_with_durations): #[track, duration]
-    # fig, ax = plt.subplots()
-    # fig1, ax1 = plt.subplots()
-    # fig2, ax2 = plt.subplots()
+
+def fourier_transform(*tracks_with_durations): # массив из [track, duration]
+    """
+    функция разложения в спектр по Фурье
+    возвращает массив из пар спектров - обычного и очищенного
+    """
     yf_mas = [] # массив из пар обычного и сглаженного Фурье для каждого из треков
+    
     for channel in tracks_with_durations:
         d_channel = copy.copy(channel[0])
-        # Number of samplepoints
+        N = len(d_channel) # количество сэмплов
+        dT = channel[1] / N # временной промежуток между соседними сэмплами
+        yf = scipy.fft.rfft(d_channel) 
+        tf = np.linspace(0.0, dT*N, N)
+        xf = np.linspace(0.0, 1.0/(2.0*dT), N//2)
+        yf_mas.append([yf, smooth(yf)]) # добавляем пару в общий массив
+        
+    return yf_mas
+
+
+def fourier_transform_graph(plt_show_usage=False, *tracks_with_durations): #[track, duration]
+    """
+    то же, что и функция выше, но с рисованием графиков
+    plt_show_usage (bool type) отвечает за то, вызвать ли plt.show() сразу из функции или нет
+    """
+    fig, ax = plt.subplots()
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    
+    yf_mas = [] # массив из пар обычного и сглаженного Фурье для каждого из треков
+    
+    for channel in tracks_with_durations:
+        d_channel = copy.copy(channel[0])
+        # количество сэмплов
         N = len(d_channel)
-        # sample spacing
+        # временной промежуток между соседними сэмплами
         dT = channel[1] / N
+        
         yf = scipy.fft.rfft(d_channel)
         tf = np.linspace(0.0, dT*N, N)
         xf = np.linspace(0.0, 1.0/(2.0*dT), N//2)
         yf_mas.append([yf, smooth(yf)]) # добавляем пару в общий массив
-        """
+        
         # рисуем график самого отрезка
         ax.plot(tf, d_channel)
         ax.set_title('d_channel')
+        
         # рисуем разложение по спектру
         ax1.plot(xf[0:N//4], 2.0/N * np.abs(yf[0:N//4])) 
         ax1.set_title('spectrum')
 
+        # рисуем очищенное разложение по спектру
         ax2.plot(xf[0:N//4], 2.0/N * np.abs(smooth(yf)[0:N//4])) 
         ax2.set_title('smooth spectrum')
-        #zf = scipy.fft.ifft(yf) # в идеале эта функция обратно преобразовывает
-        #ax2.plot(tf, zf) # рисуем график обратного преобразования
-        """
-        
-    # plt.show()
+
+        if plt_show_usage:
+            plt.show()
+
     return yf_mas
 
+
 def reverse_transform(yf, duration):
+    """
+    функция восстановления аудиодорожки по спектру
+    """
     channel_zf =  scipy.fft.irfft(yf)
     N = len(channel_zf)
     dT = duration / N
     tf = np.linspace(0.0, dT*N, N)
+    
+    return channel_zf
+
+
+def reverse_transform_graph(yf, duration, plt_show_usage=False):
     """
+    то же, что и выше, но с рисование графиков
+    plt_show_usage (bool type) отвечает за то, вызвать ли plt.show() сразу из функции или нет
+    """
+    channel_zf =  scipy.fft.irfft(yf)
+    N = len(channel_zf)
+    dT = duration / N
+    tf = np.linspace(0.0, dT*N, N)
+    
     fig2, ax2 = plt.subplots()
     ax2.plot(tf, channel_zf)
     ax2.set_title('reverse_transform')
-    plt.show()
-    """
+
+    if plt_show_usage:
+        plt.show()
     
     return channel_zf
